44c06905e733fd5e50fec6d1cabaa4cb717713bb
yannick DOLLOU <yannick.dollou@axa.fr>
2023-06-16 14:30:05 +0200

    mise en place des tests controller suite et fin

--- src/test/java/com/openclassrooms/mediscreen/controllerTest/PatientControllerTest.java
+++ src/test/java/com/openclassrooms/mediscreen/controllerTest/PatientControllerTest.java
@@ -3,14 +3,16 @@ package com.openclassrooms.mediscreen.controllerTest;
 import com.openclassrooms.mediscreen.constant.Genre;
 import com.openclassrooms.mediscreen.entities.Patient;
 import com.openclassrooms.mediscreen.services.PatientService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
 
@@ -20,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class PatientControllerTest {
 
     @Autowired
@@ -28,6 +31,7 @@ public class PatientControllerTest {
     private Jackson2ObjectMapperBuilder objectMapper;
     @Autowired
     PatientService patientService;
+
 
     @Test
     public void testCreatePatient() throws Exception {
@@ -50,10 +54,17 @@ public class PatientControllerTest {
 
     @Test
     public void testGetPatientById() throws Exception {
-        Long patientId = 1L;
+        Patient patient = new Patient();
+        patient.setPrenom("Jean");
+        patient.setNom("Dupont");
+        patient.setDateDeNaissance(LocalDate.of(2000, 12, 12));
+        patient.setGenre(Genre.F);
+        patient.setAdressePostale("10 rue du test");
+        patient.setNumeroDeTelephone("123-456-789");
+        Patient createdPatient = patientService.savePatient(patient);
 
         // Effectuez la requête GET avec l'ID du patient
-        mockMvc.perform(get("/apipatient/patients/{id}", patientId))
+        mockMvc.perform(get("/apipatient/patients/{id}", createdPatient.getId()))
                 .andExpect(status().isOk());
     }
 
@@ -75,7 +86,7 @@ public class PatientControllerTest {
     public void testUpdatePatient() throws Exception {
         // Créer un patient existant avec les attributs à mettre à jour
         Patient existingPatient = new Patient();
-        existingPatient.setId(1L);
+        existingPatient.setId(2L);
         existingPatient.setPrenom("John");
         existingPatient.setNom("Doe");
         existingPatient.setDateDeNaissance(LocalDate.of(1990, 1, 1));
@@ -84,17 +95,18 @@ public class PatientControllerTest {
         existingPatient.setGenre(Genre.M);
 
         // Enregistrer le patient existant dans la base de données (par exemple, via le repository)
+        Patient savedPatient = patientService.savePatient(existingPatient);
 
         // Effectuer la requête PUT sur l'endpoint /patients/{id} avec les données mises à jour
-        mockMvc.perform(put("/apipatient/patients/{id}", existingPatient.getId())
-                        .content(objectMapper.build().writeValueAsString(existingPatient))
+        mockMvc.perform(put("/apipatient/patients/{id}", savedPatient.getId())
+                        .content(objectMapper.build().writeValueAsString(savedPatient))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.prenom").value(existingPatient.getPrenom()))
                 .andExpect(jsonPath("$.nom").value(existingPatient.getNom()))
-                .andExpect(jsonPath("$.dateDeNaissance").value("01/01/1990")) // Utiliser le formatter pour formater la date
+                .andExpect(jsonPath("$.dateDeNaissance").value("01/01/1990"))
                 .andExpect(jsonPath("$.adressePostale").value(existingPatient.getAdressePostale()))
                 .andExpect(jsonPath("$.numeroDeTelephone").value(existingPatient.getNumeroDeTelephone()))
                 .andExpect(jsonPath("$.genre").value(existingPatient.getGenre().toString()));
